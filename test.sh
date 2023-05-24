aca4386852ad0144d12ce902093107843cf4efb7
HMarvellNW <harry.marvell@nationwide.co.uk>
2023-05-24 14:33:55 +0100

    More Testing

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
- curl http://0.0.0.0:8080 
-# then
-#   echo "Nginx server is running and accessible."
-# else
-#   echo "Failed to access Nginx server."
-#   exit 1
-# fi
+# Check if Nginx is running
+if curl --output /dev/null --silent --fail http://0.0.0.0:8080; then
+  echo "Nginx server is running and accessible."
+else
+  echo "Failed to access Nginx server."
+  exit 1
+fi
