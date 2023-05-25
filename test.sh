c8561758656a8df0a4eb87138286ee8fffcd8d00
HMarvellNW <harry.marvell@nationwide.co.uk>
2023-05-25 09:47:34 +0100

    Add IF condition to testing

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker exec my-nginx-container curl localhost:80
+output=$(docker exec my-nginx-container curl localhost:80)
+
+if [[ $? -eq 0 ]]; then
+    echo "Test passed: Connection successful"
+    echo "Output: $output"
+else
+    echo "Test failed: Connection unsuccessful"
+    echo "Error message: $output"
+fi
